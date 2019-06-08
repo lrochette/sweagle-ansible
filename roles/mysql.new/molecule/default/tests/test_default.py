a6e2874844edd170ae3abbdddbcfec7201bb5cd8
Dimitris FINAS <dfinas@gmail.com>
2019-06-08 18:12:55 +0200

    add new role

---
+++ roles/mysql.new/molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_mysql_port(host):
+    mysql = host.addr("127.0.0.1")
+    mysql.port(3306).is_reachable
